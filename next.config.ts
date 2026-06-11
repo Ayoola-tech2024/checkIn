9f7d5dd0afe1a5e3b106948993e582d1c7dbcf63
Z User <z@container>
2026-06-11 08:24:11 +0000

    b9353120-00aa-43b6-9b60-33dfc810bfbc

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,13 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://21.0.11.32:81",
     "http://21.0.11.32:3000",
+    "http://21.0.17.107:3000",
+    "http://21.0.17.107:81",
+    "http://21.0.17.107",
     "http://localhost:3000",
+    "http://localhost:81",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:81",
     "https://preview-chat-e4af0c47-5c37-4365-98cf-7b7c08f61c4c.space-z.ai",
   ],
 };
